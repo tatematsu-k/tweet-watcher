2a6a229e992d1d0512ccae2e79d6eabc26d6818a
tatematsu-k <ynu1264@gmail.com>
2025-07-01 13:09:39 +0900

    Integrationクラス導入: Slack以外も対応可能な構造にリファクタ

---
+++ settings_api/integration_slack.py
@@ -0,0 +1,17 @@
+from common.integration_base import IntegrationBase
+from urllib.parse import parse_qs
+
+class SlackIntegration(IntegrationBase):
+    def parse_input(self, event):
+        body = event.get("body", "")
+        params = {k: v[0] for k, v in parse_qs(body).items()}
+        text = params.get("text", "")
+        args = text.strip().split()
+        return args
+
+    def build_response(self, message):
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "text/plain"},
+            "body": message
+        }
